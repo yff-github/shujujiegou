1216363686e146288f346c1d2726d06c4c93d50a
凡凡 叶 <1456883297@qq.com>
2022-11-05 14:56:34 +0800

    aa1

--- shujujiegou/main.cpp
+++ shujujiegou/main.cpp
@@ -37,36 +37,6 @@ void shunxubiao1() {
 //    struct LNode* next;
 //}*LNode, LNode;
 
-//void lianbiao() {
-//    LinkList l;
-//    HeadInstert(l);
-//    print(l);
-//    
-//}
-//typedef struct LinkList
-//{
-//    struct LNode* front, * rear;
-//}*LinkList, LinkList;
-//
-//typedef struct LNode
-//{
-//    ElemType data;
-//    struct LNode* next;
-//}*LNode, LNode;
-
-
-//void HeadInstert(LinkList& l)
-//{
-//    int nums;
-//    LNode* s = (LNode*)malloc(sizeof(LNode));
-//    l->front = s;
-//    std::cin >> nums;
-//    while (nums != 9999)
-//    {
-//
-//    }
-//    l->rear = s;
-//}
 
 
 
@@ -88,44 +58,12 @@ int main()
     */
     int a[12] = { 0,1,2,3,4,5,6,7,8,9,10,11 };
     int b[12] = { 0,4,3,5,2,7,6,8,1,10,9,11 };
-    BitTree t=BuildTree(a, b, 1, 11, 1, 11);
+    BitTree t = BuildTree(a, b, 1, 11, 1, 11);
     PostOrder(t);
     printf("\n");
-    int s=Iscompleate(t);
+    int s = Iscompleate(t);
     printf("%d", s);
 
-//void HeadInstert(LinkList& l)
-//{
-//    int nums;
-//    LNode* s = (LNode*)malloc(sizeof(LNode));
-//    l->front = s;
-//    std::cin >> nums;
-//    while (nums != 9999)
-//    {
-//
-//    }
-//    l->rear = s;
-//}
-    
-
-
-}
-
-
-int main()
-{
-    BitTree a;
-    int b[10];
-    for (int i = 0; i < 10; i++)
-        b[i] = i;
-  /*  creatBinTree(a);*/
-    a = NULL;
-    To_Tree(a, b, 0, 9);
-   // LevelOrder(a);
-    //InOrder(a);
-    ProOrder(a);
-    printf("\n");
-    ProOrder_1(a);
 }
 
 
--- shujujiegou/shu.cpp
+++ shujujiegou/shu.cpp
@@ -55,56 +55,6 @@ void PostOrder_1_1(BitTree p)
 }
 
 
-//void buildtree(bittree &t, int a[], int begin, int end)
-//{
-//	if (begin > end)
-//		return;
-//	int mid = (begin + end) / 2;
-//	if (bittree == null)
-//	{
-//		bittree = (node*)malloc(sizeof(node));
-//		bittree->data = a[mid];
-//		bittree->left = null;
-//		bittree->right = null;
-//	}
-//	buildtree(bittree->, a, begin, mid - 1);
-//	buildtree(bittree->right, a, mid + 1, end);
-//}
-
-void To_Tree(BitTree&t, int a[], int begin, int end)
-{
-	if (begin > end)
-		return;
-	int mid = (begin + end) / 2;
-	if (t == NULL)
-	{
-		t = (BitNode*)malloc(sizeof(BitNode));
-		t->data = a[mid];
-		t->lchild = NULL;
-		t->rchild = NULL;
-	}
-	To_Tree(t->lchild, a, begin, mid - 1);
-	To_Tree(t->rchild, a, mid + 1, end);
-}
-
-
-
-bool creatBinTree(BitTree& BT)
-{
-	printf("请输入当前节点数据\n");
-	char ch;
-	scanf(" %c", &ch);
-	if (ch == '*')
-		BT = NULL;
-	else
-	{
-		BT = (BitTree)malloc(sizeof(BitNode));
-		BT->data = ch - '0';
-		creatBinTree(BT->lchild);
-		creatBinTree(BT->rchild);
-	}
-	return true;
-}
 
 
 //void buildtree(bittree &t, int a[], int begin, int end)
@@ -217,7 +167,7 @@ void PostOrder_1(BitTree p)
 		printf("\n");
 	}
 
-	}
+	
 }
 
 
@@ -308,6 +258,8 @@ bool Iscompleate(BitTree t)
 	}
 	return 1;
 }
+
+
 
 
 
